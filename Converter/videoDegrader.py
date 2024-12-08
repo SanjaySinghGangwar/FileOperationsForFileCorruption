2b991d6a124736b28e38d9632e639b381530e985
Sanjay S Gangwar <contact.gangwar@gmail.com>
2024-12-08 20:10:43 +0530

    Video Degrader

--- Converter/videoDegrader.py
+++ Converter/videoDegrader.py
@@ -1,6 +1,5 @@
 import logging
 import os
-
 import ffmpeg
 from tqdm import tqdm  # Import tqdm for progress bar
 
@@ -34,33 +33,34 @@ def convert_to_480p(video_path, output_path):
         logging.error(f"Error converting {video_path}: {e}")
 
 
-def process_videos(input_directory, output_directory):
+def process_videos(watch_directory):
     """
-    Processes all video files in the input directory.
-    Converts videos with resolution greater than 720p and saves them in the output directory.
+    Processes all video files in the watch directory.
+    Converts videos with resolution greater than 480p and replaces the original files.
     """
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-
-    video_files = [f for f in os.listdir(input_directory) if f.endswith(('.mp4', '.mkv', '.avi', '.mov'))]
+    video_files = [f for f in os.listdir(watch_directory) if f.endswith(('.mp4', '.mkv', '.avi', '.mov'))]
 
     # Use tqdm to create a progress bar for video file processing
     for filename in tqdm(video_files, desc="Processing videos", unit="file"):
-        video_path = os.path.join(input_directory, filename)
+        video_path = os.path.join(watch_directory, filename)
         width, height = get_video_resolution(video_path)
 
         if width is None or height is None:
             logging.warning(f'Skipping {filename} due to an error reading resolution.')
             continue
 
-        if height > 500:
-            output_path = os.path.join(output_directory, f"{filename}")
-            convert_to_480p(video_path, output_path)
+        if height > 480:
+            temp_output_path = os.path.join(watch_directory, f"temp_{filename}")
+            convert_to_480p(video_path, temp_output_path)
+
+            # Replace the original file with the converted one
+            os.remove(video_path)
+            os.rename(temp_output_path, video_path)
+            logging.info(f'Replaced original {filename} with 480p version.')
         else:
-            logging.info(f'{filename} is already 420p or lower. Skipping conversion.')
+            logging.info(f'{filename} is already 480p or lower. Skipping conversion.')
 
 
 if __name__ == "__main__":
-    input_directory = "/input_Folder"  # Change to your input directory containing videos
-    output_directory = "/output_Folder"  # Directory where you want to store converted videos
-    process_videos(input_directory, output_directory)
+    watch_directory = "/Volumes/2 TB/Personal/Creative/ContentCreations/Raw/"  # Change to your watch directory
+    process_videos(watch_directory)
